c5afb15b10ed00f08065be669241b2d8b05a5429
Ashikur07 <ashik.ict.iu@gmail.com>
2025-05-03 15:24:44 +0600

    different useses of questionmark (?)

---
+++ module1/src/1.11.ts
@@ -0,0 +1,54 @@
+{
+
+// ternary operator
+// optional chaining
+// nullish coalescing operator
+
+const age: number = 15;
+if (age > 18) {
+    console.log("You are an adult");
+}
+else {
+    console.log("You are a minor");
+}
+
+// ternary operator
+const isAdult = age > 18 ? "You are an adult" : "You are a minor"; // ternary operator
+console.log(isAdult);
+
+
+// nullish coalescing operator ( null or undefined)
+const isAuthenticated = null; // nullish coalescing operator
+const result = isAuthenticated ?? "Guest"; // nullish coalescing operator
+console.log(result); // Guest
+
+
+// optional chaining operator (?.)
+const user = {
+    name: {
+        firstName: "Ashik",
+        lastName: "Sakib",
+    },
+    age: 23,
+}
+
+const midddleName = user?.name?.midddleName ?? "No middle name"; // optional chaining operator
+console.log(midddleName); // No middle name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
